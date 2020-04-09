91a5f9daffe105393a9ab9166747af64c33f2502
win10 <komalsubhra6768@gmail.com>
2020-04-09 19:51:51 -0400

    genrating geter and setter in CRA Customer classs

--- app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Model/CRACustomer.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Model/CRACustomer.java
@@ -51,9 +51,97 @@ public class CRACustomer implements Parcelable {
         this.grossIncome = grossIncome;
         this.rrspContributed = rrspContributed;
     }
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
 
-   
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(SIN);
+        dest.writeString(firstName);
+        dest.writeString(lastName);
+        if (grossIncome == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeFloat(grossIncome);
+        }
+        if (rrspContributed == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeFloat(rrspContributed);
+        }
+    }
+
+
+    /*getter setter*/
+    public String getFullName()
+    {
+        return lastName.toUpperCase() + ", " + firstName;
+    }
+
+    public String getSIN() {
+        return SIN;
+    }
+
+    public void setSIN(String SIN) {
+        this.SIN = SIN;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(Float grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public double getRrspContributed() {
+        return rrspContributed;
+    }
+
+    public void setRrspContributed(Float rrspContributed) {
+        this.rrspContributed = rrspContributed;
+    }
+
+
+
 
 
 
