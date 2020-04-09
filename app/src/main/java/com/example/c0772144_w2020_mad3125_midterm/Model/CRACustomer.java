9930492a8ba858cafa56851e3cf9243f58c9ae7d
win10 <komalsubhra6768@gmail.com>
2020-04-09 19:48:20 -0400

    Initilizing all fields in CRA Customer classs

--- app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Model/CRACustomer.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Model/CRACustomer.java
@@ -8,5 +8,14 @@ import com.example.c0772144_w2020_mad3125_midterm.Util.Gender;
 import java.time.LocalDate;
 
 public class CRACustomer implements Parcelable {
-   
+    private String SIN;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private LocalDate birthDate;
+    private Float grossIncome;
+    private Float rrspContributed;
+
+  
+
 }
