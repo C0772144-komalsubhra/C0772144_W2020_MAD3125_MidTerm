0d6cdd793d695e4deb26587b0c4414dc5513d02f
win10 <komalsubhra6768@gmail.com>
2020-04-09 20:20:26 -0400

    Creating activites to handle methods

--- app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Util/MethodsActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Util/MethodsActivity.java
@@ -5,20 +5,11 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 public class MethodsActivity {
-    private static MethodsActivity repoObj = new MethodsActivity();
+    private static MethodsActivity methodsActivity = new MethodsActivity();
     public static MethodsActivity getInstance() {
-        return repoObj;
+        return methodsActivity;
     }
 
-    public LocalDate stringToDate(String aDate)
-    {
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd-MMM-yyyy");
-        return formatter.parseLocalDate(aDate);
-    }
-
-    public String doubleFormatter(Double d)
-    {
-        return String.format("%.2f", d);
-    }
+  
 }
 
