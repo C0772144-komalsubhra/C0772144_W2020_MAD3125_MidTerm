89dc4541bf46087852deb9e80e38c3bcfa375df2
win10 <komalsubhra6768@gmail.com>
2020-04-09 20:13:28 -0400

    added background task

--- app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/c0772144_w2020_mad3125_midterm/Activities/SplashScreenActivity.java
@@ -19,5 +19,19 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new BackgroundTask().execute();
+    }
+
+
+   
 
 }
